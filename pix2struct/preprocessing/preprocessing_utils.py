0ca7f844601e78932ab9241f5bc2a747f5a73dbe
The pix2struct Authors <pix2struct-copybara@google.com>
2023-02-08 15:40:10 -0800

    Make `render_text` a bit more general and use a larger default font to improve render quality.
    PiperOrigin-RevId: 508203031

--- pix2struct/preprocessing/preprocessing_utils.py
+++ pix2struct/preprocessing/preprocessing_utils.py
@@ -26,7 +26,7 @@ from PIL import ImageDraw
 from PIL import ImageFont
 import tensorflow as tf
 
-FONT = "arial.ttf"
+DEFAULT_FONT_PATH = "arial.ttf"
 
 
 def add_int_feature(example: tf.train.Example,
@@ -103,23 +103,42 @@ def render_header(image: Image.Image, header: str) -> Image.Image:
   return new_image
 
 
-def render_text(text: str) -> Image.Image:
+def render_text(text: str,
+                text_size: int = 36,
+                text_color: str = "black",
+                background_color: str = "white",
+                left_padding: int = 5,
+                right_padding: int = 5,
+                top_padding: int = 5,
+                bottom_padding: int = 5,
+                font_path: str = DEFAULT_FONT_PATH) -> Image.Image:
   """Render text."""
   # Add new lines so that each line is no more than 80 characters.
   wrapper = textwrap.TextWrapper(width=80)
   lines = wrapper.wrap(text=text)
   wrapped_text = "\n".join(lines)
-  font = ImageFont.truetype(FONT, encoding="UTF-8", size=12)
+
+  if tf.io.gfile.exists(font_path):
+    with tf.io.gfile.GFile(font_path, "rb") as font_file:
+      font = ImageFont.truetype(font_file, encoding="UTF-8", size=text_size)
+  else:
+    font = ImageFont.truetype(font_path, encoding="UTF-8", size=text_size)
 
   # Use a temporary canvas to determine the width and height in pixels when
   # rendering the text.
-  temp_draw = ImageDraw.Draw(Image.new("RGB", (1, 1), "white"))
-  _, _, width, height = temp_draw.textbbox((0, 0), wrapped_text, font)
+  temp_draw = ImageDraw.Draw(Image.new("RGB", (1, 1), background_color))
+  _, _, text_width, text_height = temp_draw.textbbox((0, 0), wrapped_text, font)
 
   # Create the actual image with a bit of padding around the text.
-  image = Image.new("RGB", (width + 10, height + 10), "white")
+  image_width = text_width + left_padding + right_padding
+  image_height = text_height + top_padding + bottom_padding
+  image = Image.new("RGB", (image_width, image_height), background_color)
   draw = ImageDraw.Draw(image)
-  draw.text(xy=(5, 5), text=wrapped_text, fill="black", font=font)
+  draw.text(
+      xy=(left_padding, top_padding),
+      text=wrapped_text,
+      fill=text_color,
+      font=font)
   return image
 
 
@@ -134,13 +153,14 @@ def render_text_on_bounding_box(
 
   fontsize = 1
   def _can_increment_font(ratio=0.95):
-    next_font = ImageFont.truetype(FONT, encoding="UTF-8", size=fontsize + 1)
+    next_font = ImageFont.truetype(
+        DEFAULT_FONT_PATH, encoding="UTF-8", size=fontsize + 1)
     width, height = next_font.getsize(text)
     return width < ratio * (x1 - x0) and height < ratio * (y1 - y0)
 
   while _can_increment_font():
     fontsize += 1
-  font = ImageFont.truetype(FONT, encoding="UTF-8", size=fontsize)
+  font = ImageFont.truetype(DEFAULT_FONT_PATH, encoding="UTF-8", size=fontsize)
 
   draw.text(
       xy=((x0 + x1)/2, (y0 + y1)/2),
