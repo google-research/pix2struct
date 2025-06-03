67e9f3080850d063c74d65f6336ac86fe817fb04
Jake VanderPlas <vanderplas@google.com>
2025-06-03 07:47:51 -0700

    Disable pytype for errors revealed by JAX build refactor
    PiperOrigin-RevId: 766661857

--- pix2struct/models_test.py
+++ pix2struct/models_test.py
@@ -1,4 +1,4 @@
-# Copyright 2024 The pix2struct Authors.
+# Copyright 2025 The pix2struct Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ class ModelsTest(absltest.TestCase):
         partitioner=self.partitioner,
         eval_names=[],
         summary_dir=None,
-        train_state_axes=self.train_state_initializer.train_state_axes,
+        train_state_axes=self.train_state_initializer.train_state_axes,  # pytype: disable=attribute-error  # jax-api-types
         rng=jax.random.PRNGKey(0),
         learning_rate_fn=lambda x: 0.001,
         num_microbatches=1)
